819af63e0a55ab63ce0d499352c272aa9e32e2a8
kirankumarsunkati1 <sunkati@gmail.com>
2023-01-03 08:27:08 +0530

    added form input
    added form input

--- test/src/app/material-module/material-module.module.ts
+++ test/src/app/material-module/material-module.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
-const MaterialComponent = [MatButtonModule, MatIconModule, MatSidenavModule]
+
+const MaterialComponent = [MatButtonModule, MatIconModule, MatSidenavModule, MatFormFieldModule,MatInputModule]
 
 @NgModule({
   imports: [MaterialComponent],
